86708c2a4a1237c413ef95706fb66dc310474fe3
diazed787 <64755357+diazed787@users.noreply.github.com>
2025-02-20 12:23:03 -0500

    First pass at container creation script

---
+++ install_containers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Checking if AppConnector is already running"
+COUNT="$(docker ps | grep zpa-connector | wc -l)"
+echo "App Connector count is $COUNT"
+if [ $COUNT -ne 0 ]
+then
+	echo "AppConnector is already running"
+else
+	echo "Let's provision your AppConnector"
+	read -p "Enter ZPA Prov Key: " zpakey
+	docker pull zscaler/zpa-connector:latest.amd64
+	docker run -d --init \
+	--name zpa-connector \
+	--cap-add cap_net_admin \
+	--cap-add cap_net_bind_service \
+	--cap-add cap_net_raw \
+	--cap-add cap_sys_nice \
+	--cap-add cap_sys_time \
+	--cap-add cap_sys_resource \
+	--restart always \
+	-e ZPA_PROVISION_KEY="$zpakey" \
+	zscaler/zpa-connector:latest.amd64
+fi
